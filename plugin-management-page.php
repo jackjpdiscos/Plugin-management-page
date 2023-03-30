3c792c7a70b2184424975a73f11d7b134293315a
jackjpdiscos <74415816+jackjpdiscos@users.noreply.github.com>
2023-03-30 23:08:23 +0100

    Update plugin-management-page.php

--- plugin-management-page.php
+++ plugin-management-page.php
@@ -9,42 +9,50 @@
 
 // we are going to create the admin page and our Settings page for this plugin will be located under General Settings tab.
 // Add a menu for our option page
-add_action('admin_menu', 'pmp_plugin_add_settings_menu');
+add_action( 'admin_menu', 'pmp_plugin_add_settings_menu' );
+
 function pmp_plugin_add_settings_menu() {
-    add_options_page('PMP Plugin Settings', 'PMP Settings', 'manage_options', 'pmp_plugin', 'pmp_plugin_option_page');
+    add_options_page( 'PMP Plugin Settings', 'PMP Settings', 'manage_options', 'pmp_plugin', 'pmp_plugin_option_page' );
 
 }
 
 // create the option page
-function pmp_plugin_option_page(){
+function pmp_plugin_option_page() {
     ?>
 <div class="wrap" xmlns="http://www.w3.org/1999/html">
-    <h2>Plugin Management Page</h2>
-    <form action="options.php" method="post">
-        <?php
-        settings_fields('pmp_plugin_options');
-        do_settings_sections('pmp_plugin');
-        submit_button('Save Changes', 'primary');
-        ?>
-    </form>
+  <h2>Plugin Management Page</h2>
+  <form action="options.php" method="post">
+    <?php
+    settings_fields( 'pmp_plugin_options' );
+    do_settings_sections( 'pmp_plugin' );
+    submit_button( 'Save Changes', 'primary' );
+    ?>
+  </form>
 </div>
 <?php
 }
 // register and define the settings
-    add_action('admin_init', 'pmp_plugin_admin_init');
-function pmp_plugin_admin_init(){
+add_action( 'admin_init', 'pmp_plugin_admin_init' );
+
+function pmp_plugin_validate_options( $input ) {
+    // Sanitize the input
+    $input[ 'name' ] = sanitize_text_field( $input[ 'name' ] );
+    return $input;
+}
+
+function pmp_plugin_admin_init() {
     $args = array(
-            'type'=>'string',
-        'sanitize_callback'=>'pmp_plugin_validate_options',
-        'default'=> NULL
+        'type' => 'string',
+        'sanitize_callback' => 'pmp_plugin_validate_options',
+        'default' => NULL
     );
 
     // register our settings
-    register_setting('pmp_plugin_options', 'pmp_plugin_options', $args);
+    register_setting( 'pmp_plugin_options', 'pmp_plugin_options', $args );
 
     //Add a settings section
     add_settings_section(
-            'pmp_plugin_main',
+        'pmp_plugin_main',
         'PMP Plugin Settings',
         'pmp_plugin_section_text',
         'pmp_plugin'
@@ -52,7 +60,7 @@ function pmp_plugin_admin_init(){
 
     //create our settings field for name
     add_settings_field(
-            'pmp_plugin_name',
+        'pmp_plugin_name',
         'Your Name',
         'pmp_plugin_setting_name',
         'pmp_plugin',
@@ -60,18 +68,17 @@ function pmp_plugin_admin_init(){
     );
 
     //draw section header
-    function pmp_plugin_section_text(){
+    function pmp_plugin_section_text() {
         echo '<p>Enter your settings here.</p>';
     }
 
     // Display and fill the Name form field
     function pmp_plugin_setting_name() {
-//get options 'text string' value from thr database
-        $options = get_option('pmp_plugin_option');
-        $name = $options['name'];
+        // Get the current value from the database
+        $options = get_option( 'pmp_plugin_options' );
+        $name = isset( $options[ 'name' ] ) ? $options[ 'name' ] : '';
 
-        //echo the field
-        echo "<input id='name' name='pmp_plugin_options[name]' type= 'text' value='" . esc_attr($name) . "'/>";
-
+        // Echo the field
+        echo "<input id='name' name='pmp_plugin_options[name]' type='text' value='" . esc_attr( $name ) . "'/>";
     }
 }
